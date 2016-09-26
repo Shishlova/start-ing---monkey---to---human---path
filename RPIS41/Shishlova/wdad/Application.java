8c3a9d987403c5c98e596b8935575fcda8bcefeb
unknown <U_LIVT35313@psuti.ru>
2016-09-26 15:58:22 +0400

    Added entry point

---
+++ RPIS41/Shishlova/wdad/Application.java
@@ -0,0 +1,10 @@
+package RPIS41.Shishlova.wdad;
+
+/**
+ * Created by U_LIVT35313 on 26.09.2016.
+ */
+public class Application {
+    public static void main(String[] args){
+        System.out.println("I'm Alena Shishlova, and I'm not a monkey");
+    }
+}
